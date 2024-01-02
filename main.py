14332335558025a80584b90f9aa52fe7f609c224
watakak <155397402+watakak@users.noreply.github.com>
2024-01-02 15:48:37 +0100

    Update/Release 1.014

--- main.py
+++ main.py
@@ -3,23 +3,19 @@ import shutil
 import os
 
 def generate_custom_click_sound(file_path):
-    # Загрузка звука из файла WAV
     custom_click_sound = AudioSegment.from_wav(file_path)
     return custom_click_sound
 
 def preprocess_click_data(input_file, output_file):
-    # Копируем содержимое из исходного файла в новый файл
-    shutil.copyfile(f"{file_name}.txt", f"{file_name}.process")
+    shutil.copyfile(f'{file_name}.txt', f'{file_name}.process')
 
-    # Открываем новый файл и заменяем "0 1 1" на ""
     with open(output_file, 'r') as file:
         lines = file.readlines()[1:]
 
-    lines = [line.replace(" 0 1 1", "") for line in lines]
+    lines = [line.replace(' 0 1 1', '') for line in lines]
 
     with open(output_file, 'w') as file:
         file.writelines(lines)
-
 
 def read_click_data(file_path):
     click_data = []
@@ -29,12 +25,10 @@ def read_click_data(file_path):
             click_data.append((frame, click_state))
     return click_data
 
-
-def create_click_audio(click_data, frame_rate=144, click_sound_path="click.wav", release_sound_path="release.wav",
+def create_click_audio(click_data, frame_rate=144, click_sound_path='click.wav', release_sound_path='release.wav',
                        output_file=None):
-
     if output_file is None:
-        output_file = f"{file_name}.mp3"
+        output_file = f'{file_name}_clicks.mp3'
 
     frame_duration = 1000 / frame_rate
     audio = AudioSegment.silent(duration=int(click_data[-1][0] * frame_duration) + 100)
@@ -48,27 +42,26 @@ def create_click_audio(click_data, frame_rate=144, click_sound_path="click.wav",
         else:
             audio = audio.overlay(release_sound, position=int(frame * frame_duration))
 
-    audio.export(output_file, format="mp3")
+    audio.export(output_file, format='mp3')
 
 def start():
-
-    print('Welcome to GDClick 1.01')
+    print('Welcome to GDClick 1.014')
     print('Made by watakak')
     print('')
     file_name = input('Input only name of the txt file: ')
-    framerate = int(input("Input FPS's of the GDBot record: "))
+    framerate = int(input('Input framerate of the GDBot record: '))
 
     return file_name, framerate
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     file_name, framerate = start()
 
-    file_path = f"{file_name}.txt"
-    temp_file = f"{file_name}.process"
+    file_path = f'{file_name}.txt'
+    temp_file = f'{file_name}.process'
     frame_rate = framerate
-    click_sound_path = "click_sound1.wav"  # Путь к вашему звуку нажатия мышки
-    release_sound_path = "release_sound1.wav"  # Путь к вашему звуку отжатия мышки
-    output_file = f"{file_name}.mp3"
+    click_sound_path = 'hard_mouse_click_sound1.wav'
+    release_sound_path = 'hard_mouse_release_sound1.wav'
+    output_file = f'{file_name}_clicks.mp3'
 
     preprocess_click_data(temp_file, temp_file)
 
